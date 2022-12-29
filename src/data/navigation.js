98e793ceba141797ce3a3714ccc2c70ddbb621e3
Robert Guss <robertguss@pm.me>
2022-12-29 11:21:08 -0500

    added post on what makes astro different from other frameworks

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -50,15 +50,15 @@ export const navigation = [
     title: 'Astro (work in progress)',
     links: [
       {
-        title: '1. Overview & Intro',
-        href: '/posts/astro-static-site-generator-overview',
+        title: 'What makes Astro different from other frameworks ',
+        href: '/posts/astro-what-makes-astro-different-from-other-frameworks',
       },
       {
-        title: '2. How to install Astro and build your first site',
+        title: 'How to install Astro and build your first site',
         href: '/posts/astro-how-to-install-astro-and-build-your-first-site',
       },
       {
-        title: '3. Astro components, layouts, and pages',
+        title: 'Astro components, layouts, and pages',
         href: '/posts/astro-components-layouts-pages',
       },
     ],
