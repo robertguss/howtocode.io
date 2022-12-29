573694dca35990d1b5fd8de2c815dd970a021b97
Robert Guss <robertguss@pm.me>
2022-12-29 16:11:46 -0500

    finished all redirects and directories

--- next.config.js
+++ next.config.js
@@ -89,6 +89,262 @@ const nextConfig = {
           '/posts/the-self-taught-developer/how-to-manage-your-expectations-while-teaching-yourself-how-to-code',
         permanent: true,
       },
+      {
+        source: '/posts/nextjs-install-and-overview',
+        destination: '/posts/next-js/nextjs-install-and-overview',
+        permanent: true,
+      },
+      {
+        source: '/posts/nextjs-pages-and-pre-rendering',
+        destination: '/posts/next-js/nextjs-pages-and-pre-rendering',
+        permanent: true,
+      },
+      {
+        source: '/posts/nextjs-api-routes',
+        destination: '/posts/next-js/nextjs-api-routes',
+        permanent: true,
+      },
+      {
+        source: '/posts/go-basics-1-installing-go-on-a-mac',
+        destination: '/posts/go/go-basics-1-installing-go-on-a-mac',
+        permanent: true,
+      },
+      {
+        source: '/posts/go-basics-2-variables-in-go',
+        destination: '/posts/go/go-basics-2-variables-in-go',
+        permanent: true,
+      },
+      {
+        source: '/posts/go-basics-3-primitives-in-go',
+        destination: '/posts/go/go-basics-3-primitives-in-go',
+        permanent: true,
+      },
+      {
+        source: '/posts/go-basics-4-constants-in-go',
+        destination: '/posts/go/go-basics-4-constants-in-go',
+        permanent: true,
+      },
+      {
+        source: '/posts/go-basics-5-arrays-and-slices-in-go',
+        destination: '/posts/go/go-basics-5-arrays-and-slices-in-go',
+        permanent: true,
+      },
+      {
+        source: '/posts/go-basics-6-maps-in-go',
+        destination: '/posts/go/go-basics-6-maps-in-go',
+        permanent: true,
+      },
+      {
+        source: '/posts/go-basics-7-structs-in-go',
+        destination: '/posts/go/go-basics-7-structs-in-go',
+        permanent: true,
+      },
+      {
+        source: '/posts/go-basics-8-control-flow-in-go',
+        destination: '/posts/go/go-basics-8-control-flow-in-go',
+        permanent: true,
+      },
+      {
+        source: '/posts/go-basics-9-loops-in-go',
+        destination: '/posts/go/go-basics-9-loops-in-go',
+        permanent: true,
+      },
+      {
+        source: '/posts/go-basics-10-pointers-in-go',
+        destination: '/posts/go/go-basics-10-pointers-in-go',
+        permanent: true,
+      },
+      {
+        source: '/posts/go-basics-11-functions-in-go',
+        destination: '/posts/go/go-basics-11-functions-in-go',
+        permanent: true,
+      },
+      {
+        source: '/posts/elixir-basics-basic-data-types',
+        destination: '/posts/elixir/elixir-basics-basic-data-types',
+        permanent: true,
+      },
+      {
+        source: '/posts/elixir-basics-lists',
+        destination: '/posts/elixir/elixir-basics-lists',
+        permanent: true,
+      },
+      {
+        source: '/posts/elixir-basics-tuples',
+        destination: '/posts/elixir/elixir-basics-tuples',
+        permanent: true,
+      },
+      {
+        source: '/posts/elixir-basics-keyword-lists',
+        destination: '/posts/elixir/elixir-basics-keyword-lists',
+        permanent: true,
+      },
+      {
+        source: '/posts/elixir-basics-maps',
+        destination: '/posts/elixir/elixir-basics-maps',
+        permanent: true,
+      },
+      {
+        source: '/posts/cypress-environment-variables',
+        destination: '/posts/cypress/cypress-environment-variables',
+        permanent: true,
+      },
+      {
+        source: '/posts/real-world-testing-with-cypress',
+        destination: '/posts/cypress/real-world-testing-with-cypress',
+        permanent: true,
+      },
+      {
+        source: '/posts/cypress-real-world-app',
+        destination: '/posts/cypress/cypress-real-world-app',
+        permanent: true,
+      },
+      {
+        source: '/posts/how-to-setup-algolia-doc-search',
+        destination: '/posts/algolia/how-to-setup-algolia-doc-search',
+        permanent: true,
+      },
+      {
+        source: '/posts/how-to-create-developer-screencasts-with-keynote',
+        destination:
+          '/posts/developer-experience/how-to-create-developer-screencasts-with-keynote',
+        permanent: true,
+      },
+      {
+        source: '/posts/what-is-zettelkasten',
+        destination: '/posts/writing-pkm/what-is-zettelkasten',
+        permanent: true,
+      },
+      {
+        source: '/posts/why-writing-is-important-for-software-developers',
+        destination:
+          '/posts/writing-pkm/why-writing-is-important-for-software-developers',
+        permanent: true,
+      },
+      {
+        source: '/posts/jamstack-eleventy-memberstack',
+        destination: '/posts/jamstack/eleventy-memberstack',
+        permanent: true,
+      },
+      {
+        source: '/posts/jamstack-gatsby-stripe-ecommerce',
+        destination: '/posts/jamstack/gatsby-stripe-ecommerce',
+        permanent: true,
+      },
+      {
+        source: '/posts/jamstack-hugo-netlify-zapier',
+        destination: '/posts/jamstack/hugo-netlify-zapier',
+        permanent: true,
+      },
+      {
+        source: '/posts/adonis-js-1-intro-and-setup',
+        destination: '/posts/adonis-js/adonis-js-1-intro-and-setup',
+        permanent: true,
+      },
+      {
+        source: '/posts/adonis-js-2-routes-layouts',
+        destination: '/posts/adonis-js/adonis-js-2-routes-layouts',
+        permanent: true,
+      },
+      {
+        source: '/posts/adonis-js-3-migrations-models',
+        destination: '/posts/adonis-js/adonis-js-3-migrations-models',
+        permanent: true,
+      },
+      {
+        source: '/posts/adonis-js-4-seeds-factories',
+        destination: '/posts/adonis-js/adonis-js-4-seeds-factories',
+        permanent: true,
+      },
+      {
+        source: '/posts/adonis-js-5-user-authentication',
+        destination: '/posts/adonis-js/adonis-js-5-user-authentication',
+        permanent: true,
+      },
+      {
+        source: '/posts/tailwind-css-travel-site-1-installing-tailwind-css',
+        destination:
+          '/posts/tailwind-css/travel-site-1-installing-tailwind-css',
+        permanent: true,
+      },
+      {
+        source: '/posts/tailwind-css-travel-site-2-navigation-and-hero',
+        destination: '/posts/tailwind-css/travel-site-2-navigation-and-hero',
+        permanent: true,
+      },
+      {
+        source: '/posts/tailwind-css-travel-site-3-quote-form',
+        destination: '/posts/tailwind-css/travel-site-3-quote-form',
+        permanent: true,
+      },
+      {
+        source: '/posts/tailwind-css-travel-site-4-popular-destinations',
+        destination: '/posts/tailwind-css/travel-site-4-popular-destinations',
+        permanent: true,
+      },
+      {
+        source: '/posts/tailwind-css-travel-site-5-features-section',
+        destination: '/posts/tailwind-css/travel-site-5-features-section',
+        permanent: true,
+      },
+      {
+        source: '/posts/tailwind-css-travel-site-6-callouts-section',
+        destination: '/posts/tailwind-css/travel-site-6-callouts-section',
+        permanent: true,
+      },
+      {
+        source: '/posts/tailwind-css-travel-site-7-testimonial-section',
+        destination: '/posts/tailwind-css/travel-site-7-testimonial-section',
+        permanent: true,
+      },
+      {
+        source: '/posts/tailwind-css-travel-site-8-footer-purgecss',
+        destination: '/posts/tailwind-css/travel-site-8-footer-purgecss',
+        permanent: true,
+      },
+      {
+        source: '/posts/how-to-deploy-statamic-3-to-digitalocean-app-platform',
+        destination:
+          '/posts/statamic/how-to-deploy-statamic-3-to-digitalocean-app-platform',
+        permanent: true,
+      },
+      {
+        source: '/posts/how-to-customize-your-sitemap-in-hugo',
+        destination: '/posts/hugo/how-to-customize-your-sitemap-in-hugo',
+        permanent: true,
+      },
+      {
+        source: '/posts/understanding-closures-in-javascript',
+        destination: '/posts/javascript/understanding-closures-in-javascript',
+        permanent: true,
+      },
+      {
+        source: '/posts/looping-through-objects-in-javascript',
+        destination: '/posts/javascript/looping-through-objects-in-javascript',
+        permanent: true,
+      },
+      {
+        source: '/posts/understanding-hoisting-in-javascript',
+        destination: '/posts/javascript/understanding-hoisting-in-javascript',
+        permanent: true,
+      },
+      {
+        source: '/posts/how-to-console-log-better-in-javascript',
+        destination:
+          '/posts/javascript/how-to-console-log-better-in-javascript',
+        permanent: true,
+      },
+      {
+        source: '/posts/how-to-compare-dates-in-javascript',
+        destination: '/posts/javascript/how-to-compare-dates-in-javascript',
+        permanent: true,
+      },
+      {
+        source: '/posts/understanding-map-filter-and-reduce-in-javascript',
+        destination:
+          '/posts/javascript/understanding-map-filter-and-reduce-in-javascript',
+        permanent: true,
+      },
     ]
   },
 }
--- src/data/navigation.js
+++ src/data/navigation.js
@@ -80,15 +80,15 @@ export const navigation = [
     links: [
       {
         title: '1. Install and overview',
-        href: '/posts/nextjs-install-and-overview',
+        href: '/posts/next-js/install-and-overview',
       },
       {
         title: '2. Pages and pre-rendering',
-        href: '/posts/nextjs-pages-and-pre-rendering',
+        href: '/posts/next-js/pages-and-pre-rendering',
       },
       {
         title: '3. API routes',
-        href: '/posts/nextjs-api-routes',
+        href: '/posts/next-js/api-routes',
       },
     ],
   },
@@ -97,47 +97,47 @@ export const navigation = [
     links: [
       {
         title: '1. Installing Go on a Mac',
-        href: '/posts/go-basics-1-installing-go-on-a-mac',
+        href: '/posts/go/go-basics-1-installing-go-on-a-mac',
       },
       {
         title: '2. Variables',
-        href: '/posts/go-basics-2-variables-in-go',
+        href: '/posts/go/go-basics-2-variables-in-go',
       },
       {
         title: '3. Primitives',
-        href: '/posts/go-basics-3-primitives-in-go',
+        href: '/posts/go/go-basics-3-primitives-in-go',
       },
       {
         title: '4. Constants',
-        href: '/posts/go-basics-4-constants-in-go',
+        href: '/posts/go/go-basics-4-constants-in-go',
       },
       {
         title: '5. Arrays & Slices',
-        href: '/posts/go-basics-5-arrays-and-slices-in-go',
+        href: '/posts/go/go-basics-5-arrays-and-slices-in-go',
       },
       {
         title: '6. Maps',
-        href: '/posts/go-basics-6-maps-in-go',
+        href: '/posts/go/go-basics-6-maps-in-go',
       },
       {
         title: '7. Structs',
-        href: '/posts/go-basics-7-structs-in-go',
+        href: '/posts/go/go-basics-7-structs-in-go',
       },
       {
         title: '8. Control flow',
-        href: '/posts/go-basics-8-control-flow-in-go',
+        href: '/posts/go/go-basics-8-control-flow-in-go',
       },
       {
         title: '9. Loops',
-        href: '/posts/go-basics-9-loops-in-go',
+        href: '/posts/go/go-basics-9-loops-in-go',
       },
       {
         title: '10. Pointers',
-        href: '/posts/go-basics-10-pointers-in-go',
+        href: '/posts/go/go-basics-10-pointers-in-go',
       },
       {
         title: '11. Functions',
-        href: '/posts/go-basics-11-functions-in-go',
+        href: '/posts/go/go-basics-11-functions-in-go',
       },
     ],
   },
@@ -146,23 +146,23 @@ export const navigation = [
     links: [
       {
         title: '1. Basic Data Types',
-        href: '/posts/elixir-basics-basic-data-types',
+        href: '/posts/elixir/elixir-basics-basic-data-types',
       },
       {
         title: '2. Lists',
-        href: '/posts/elixir-basics-lists',
+        href: '/posts/elixir/elixir-basics-lists',
       },
       {
         title: '3. Tuples',
-        href: '/posts/elixir-basics-tuples',
+        href: '/posts/elixir/elixir-basics-tuples',
       },
       {
         title: '4. Keyword Lists',
-        href: '/posts/elixir-basics-keyword-lists',
+        href: '/posts/elixir/elixir-basics-keyword-lists',
       },
       {
         title: '5. Maps',
-        href: '/posts/elixir-basics-maps',
+        href: '/posts/elixir/elixir-basics-maps',
       },
     ],
   },
@@ -171,15 +171,15 @@ export const navigation = [
     links: [
       {
         title: 'How to use environment variables in Cypress',
-        href: '/posts/cypress-environment-variables',
+        href: '/posts/cypress/cypress-environment-variables',
       },
       {
         title: 'Real World Testing with Cypress',
-        href: '/posts/real-world-testing-with-cypress',
+        href: '/posts/cypress/real-world-testing-with-cypress',
       },
       {
         title: 'Real World App',
-        href: '/posts/cypress-real-world-app',
+        href: '/posts/cypress/cypress-real-world-app',
       },
     ],
   },
@@ -188,16 +188,16 @@ export const navigation = [
     links: [
       {
         title: 'How to setup Algolia DocSearch',
-        href: '/posts/how-to-setup-algolia-doc-search',
+        href: '/posts/algolia/how-to-setup-algolia-doc-search',
       },
     ],
   },
   {
-    title: 'Teaching',
+    title: 'Developer Experience',
     links: [
       {
         title: 'How to Create Developer Screencasts with Keynote',
-        href: '/posts/how-to-create-developer-screencasts-with-keynote',
+        href: '/posts/developer-experience/how-to-create-developer-screencasts-with-keynote',
       },
     ],
   },
@@ -206,11 +206,11 @@ export const navigation = [
     links: [
       {
         title: 'What is Zettelkasten?',
-        href: '/posts/what-is-zettelkasten',
+        href: '/posts/writing-pkm/what-is-zettelkasten',
       },
       {
         title: 'Why Writing is Important for Software Developers',
-        href: '/posts/why-writing-is-important-for-software-developers',
+        href: '/posts/writing-pkm/why-writing-is-important-for-software-developers',
       },
     ],
   },
@@ -219,15 +219,15 @@ export const navigation = [
     links: [
       {
         title: 'Membership site with Eleventy & Memberstack',
-        href: '/posts/jamstack-eleventy-memberstack',
+        href: '/posts/jamstack/eleventy-memberstack',
       },
       {
         title: 'Ecommerce site with Gatsby & Stripe Checkout',
-        href: '/posts/jamstack-gatsby-stripe-ecommerce',
+        href: '/posts/jamstack/gatsby-stripe-ecommerce',
       },
       {
         title: 'Blog with Hugo, Netlify CMS and Zapier',
-        href: '/posts/jamstack-hugo-netlify-zapier',
+        href: '/posts/jamstack/hugo-netlify-zapier',
       },
     ],
   },
@@ -236,23 +236,23 @@ export const navigation = [
     links: [
       {
         title: '1. Intro and Setup',
-        href: '/posts/adonis-js-1-intro-and-setup',
+        href: '/posts/adonis-js/1-intro-and-setup',
       },
       {
         title: '2. Routes, Layouts, Controllers',
-        href: '/posts/adonis-js-2-routes-layouts',
+        href: '/posts/adonis-js/2-routes-layouts',
       },
       {
         title: '3. Migrations, Models, Forms',
-        href: '/posts/adonis-js-3-migrations-models',
+        href: '/posts/adonis-js/3-migrations-models',
       },
       {
         title: '4. Seeds & Factories',
-        href: '/posts/adonis-js-4-seeds-factories',
+        href: '/posts/adonis-js/4-seeds-factories',
       },
       {
         title: '5. User Authentication',
-        href: '/posts/adonis-js-5-user-authentication',
+        href: '/posts/adonis-js/5-user-authentication',
       },
     ],
   },
@@ -261,35 +261,35 @@ export const navigation = [
     links: [
       {
         title: '1. Installing Tailwind CSS',
-        href: '/posts/tailwind-css-travel-site-1-installing-tailwind-css',
+        href: '/posts/tailwind-css/travel-site-1-installing-tailwind-css',
       },
       {
         title: '2. Navigation & Hero',
-        href: '/posts/tailwind-css-travel-site-2-navigation-and-hero',
+        href: '/posts/tailwind-css/travel-site-2-navigation-and-hero',
       },
       {
         title: '3. Quote Form',
-        href: '/posts/tailwind-css-travel-site-3-quote-form',
+        href: '/posts/tailwind-css/travel-site-3-quote-form',
       },
       {
         title: '4. Popular Destinations',
-        href: '/posts/tailwind-css-travel-site-4-popular-destinations',
+        href: '/posts/tailwind-css/travel-site-4-popular-destinations',
       },
       {
         title: '5. Features Section',
-        href: '/posts/tailwind-css-travel-site-5-features-section',
+        href: '/posts/tailwind-css/travel-site-5-features-section',
       },
       {
         title: '6. Callouts Section',
-        href: '/posts/tailwind-css-travel-site-6-callouts-section',
+        href: '/posts/tailwind-css/travel-site-6-callouts-section',
       },
       {
         title: '7. Testimonial Section',
-        href: '/posts/tailwind-css-travel-site-7-testimonial-section',
+        href: '/posts/tailwind-css/travel-site-7-testimonial-section',
       },
       {
         title: '8. Footer Section & PurgeCSS',
-        href: '/posts/tailwind-css-travel-site-8-footer-purgecss',
+        href: '/posts/tailwind-css/travel-site-8-footer-purgecss',
       },
     ],
   },
@@ -298,7 +298,7 @@ export const navigation = [
     links: [
       {
         title: 'How to deploy Statamic 3 to Digitalocean App platform',
-        href: '/posts/how-to-deploy-statamic-3-to-digitalocean-app-platform',
+        href: '/posts/statamic/how-to-deploy-statamic-3-to-digitalocean-app-platform',
       },
     ],
   },
@@ -307,7 +307,7 @@ export const navigation = [
     links: [
       {
         title: 'How to customize your sitemap in Hugo',
-        href: '/posts/how-to-customize-your-sitemap-in-hugo',
+        href: '/posts/hugo/how-to-customize-your-sitemap-in-hugo',
       },
     ],
   },
@@ -316,27 +316,27 @@ export const navigation = [
     links: [
       {
         title: 'Scope and Closures',
-        href: '/posts/understanding-closures-in-javascript',
+        href: '/posts/javascript/understanding-closures-in-javascript',
       },
       {
         title: 'Looping through objects',
-        href: '/posts/looping-through-objects-in-javascript',
+        href: '/posts/javascript/looping-through-objects-in-javascript',
       },
       {
         title: 'Hoisting',
-        href: '/posts/understanding-hoisting-in-javascript',
+        href: '/posts/javascript/understanding-hoisting-in-javascript',
       },
       {
         title: 'How to console log better',
-        href: '/posts/how-to-console-log-better-in-javascript',
+        href: '/posts/javascript/how-to-console-log-better-in-javascript',
       },
       {
         title: 'How to compare dates in JavaScript',
-        href: '/posts/how-to-compare-dates-in-javascript',
+        href: '/posts/javascript/how-to-compare-dates-in-javascript',
       },
       {
         title: 'Map, Filter & Reduce',
-        href: '/posts/understanding-map-filter-and-reduce-in-javascript',
+        href: '/posts/javascript/understanding-map-filter-and-reduce-in-javascript',
       },
     ],
   },
