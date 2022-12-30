435f3f1e495faa01e22d0ec241db1b54f37db1c2
Robert Guss <robertguss@pm.me>
2022-12-29 22:48:40 -0500

    cost filtering is working, but the implementation is ghetto son

--- src/pages/learn.jsx
+++ src/pages/learn.jsx
@@ -3,13 +3,33 @@ import CostSelect from '@/components/learn/CostSelect'
 import learnResources from '../data/learn-resources.json'
 
 export default function Learn() {
+  const [allResources, setAllResources] = useState(learnResources)
   const [resources, setResources] = useState(learnResources)
+  const [selectedOption, setSelectedOption] = useState('')
+  const freeResources = resources.filter((resource) => resource.Cost === 'Free')
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value)
+
+    if (event.target.value === 'Paid & Free') {
+      setResources(allResources)
+    } else {
+      setResources(
+        allResources.filter((resource) => resource.Cost === event.target.value)
+      )
+    }
+  }
   return (
     <>
       <h1>
-        Learn How to Code <br /> from over {learnResources.length} resources
+        Learn How to Code <br /> from over {resources.length} resources
       </h1>
-      <CostSelect />
+      {/* <CostSelect /> */}
+      <select id="options" value={selectedOption} onChange={handleChange}>
+        <option value="Paid & Free">Paid & Free</option>
+        <option value="Free">Free</option>
+        <option value="Paid">Paid</option>
+      </select>
       {resources.map((resource, index) => (
         <div key={index}>
           <h2>{resource.Name}</h2>
